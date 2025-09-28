e3445aede2d9199b886e8ef239da949f905a81c1
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2025-09-28 14:16:36 +0300

    Commit #3

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("Hello World")
 print("Hello Git")
 
 print("Hello Git 2")
+
+def is_adult(age):
+    return age >= 18
+
+# Виклик функції, який поверне True або False
+can_vote = is_adult(17) 
+print(can_vote)  # Виведе: True
