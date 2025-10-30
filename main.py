6f4b8bcb28ec5fb11cd1ba6577635c1575d23691
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2025-10-30 22:40:40 +0200

    save

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-is_next = None
-num = int(input("Enter the number of points: "))
-if num >= 83:
-    print ("pass")
+work_experience = int(input("Enter your full work experience in years: "))
+
+if work_experience > 1 and work_experience <= 5:
+    developer_type = "Middle"
+elif work_experience <= 1:
+    developer_type = "Junior"
 else:
-    print ("not pass")
-    
-    
-    
+    developer_type = "Senior"
+print (developer_type)
