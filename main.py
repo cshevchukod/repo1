9ac48cfafa985c327736027e993c35337fc357a9
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2025-11-08 21:49:36 +0200

    save

--- main.py
+++ main.py
@@ -1,9 +1,169 @@
-work_experience = float(input("Enter your full work experience in years: "))
+from datetime import datetime
 
-if work_experience > 1 and work_experience <= 5:
-    developer_type = "Middle"
-elif work_experience <= 1:
-    developer_type = "Junior"
-else:
-    developer_type = "Senior"
-print (developer_type)
+current_datetime_1 = datetime.now()
+print(current_datetime_1.year)
+print(current_datetime_1.month)
+print(current_datetime_1.day)
+print(current_datetime_1.hour)
+print(current_datetime_1.minute)
+print(current_datetime_1.second)
+print(current_datetime_1.microsecond)
+print(current_datetime_1.tzinfo)
+
+print('')
+
+current_datetime_2 = datetime.now()
+print(current_datetime_2.date())
+print(current_datetime_2.time())
+
+print('')
+
+seventh_day_2019 = datetime(year=2019, month=1, day=7, hour=14)
+seventh_day_2020 = datetime(year=2020, month=1, day=7, hour=14)
+
+difference = seventh_day_2020 - seventh_day_2019
+print(difference)  # 365 days, 0:00:00
+print(difference.total_seconds())  # 31536000.0
+
+print('')
+
+from datetime import timedelta
+
+delta = timedelta(
+    days=50,
+    seconds=27,
+    microseconds=10,
+    milliseconds=29000,
+    minutes=5,
+    hours=8,
+    weeks=2
+)
+print(delta)
+
+print('')
+
+import datetime
+
+# Створення об'єкта datetime з конкретною датою і часом
+specific_datetime = datetime.datetime(year=2020, month=1, day=7, hour=14, minute=30, second=15)
+
+print(specific_datetime)  # Виведе "2020-01-07 14:30:15"
+
+print('')
+
+from datetime import datetime, timedelta, timezone
+
+seventh_day_2020 = datetime(year=2020, month=1, day=7, hour=14)
+four_weeks_interval = timedelta(weeks=4)
+
+print(seventh_day_2020)
+print(seventh_day_2020 + four_weeks_interval)  # 2020-02-04 14:00:00
+print(seventh_day_2020 - four_weeks_interval)  # 2019-12-10 14:00:00
+
+print('')
+
+# Створення об'єкта datetime
+date = datetime(year=2023, month=12, day=18)
+
+# Отримання порядкового номера
+ordinal_number = date.toordinal()
+print(f"Порядковий номер дати {date} становить {ordinal_number}")
+
+print('')
+
+# Встановлення дати спалення Москви Наполеоном (14 вересня 1812 року)
+napoleon_burns_moscow = datetime(year=1812, month=9, day=14)
+
+# Поточна дата
+current_date = datetime.now()
+
+# Розрахунок кількості днів
+days_since = current_date.toordinal() - napoleon_burns_moscow.toordinal()
+print(days_since)
+
+print('')
+
+now = datetime.now()
+
+# Конвертація datetime в timestamp
+timestamp = datetime.timestamp(now)
+print(timestamp)  # Виведе timestamp поточного часу
+
+print('')
+
+now = datetime.now()
+
+# Форматування дати і часу
+formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_date) 
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%A, %d %B %Y")
+print(formatted_date_only)
+
+# Форматування лише часу
+formatted_time_only = now.strftime("%I:%M %p")
+print(formatted_time_only)  
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%d.%m.%Y")
+print(formatted_date_only)
+
+print('')
+
+# Поточна дата та час
+now = datetime.now()
+
+# Конвертація у формат ISO 8601
+iso_format = now.isoformat()
+print(iso_format)
+print(now)
+
+print('')
+
+iso_date_string = "2023-03-14T12:39:29.992996"
+
+# Конвертація з ISO формату
+date_from_iso = datetime.fromisoformat(iso_date_string)
+print(date_from_iso)
+
+print('')
+
+# Створення об'єкта datetime
+now = datetime.now()
+
+# Отримання ISO календаря
+iso_calendar = now.isocalendar()
+
+print(f"ISO рік: {iso_calendar[0]}, ISO тиждень: {iso_calendar[1]}, ISO день тижня: {iso_calendar[2]}")
+print(iso_calendar)
+
+iso_calendar_1 = now.isoweekday()
+print(f"Сьогодні: {iso_calendar_1}")
+
+# Створення об'єкта datetime
+now = datetime.now()
+
+# Використання isoweekday() для отримання дня тижня
+day_of_week = now.isoweekday()
+
+print(f"Сьогодні: {day_of_week}")  # Поверне число від 1 до 7, що відповідає дню тижня
+
+print('')
+
+utc_time = datetime.now(timezone.utc)
+
+# Створення часової зони для Східного часового поясу (UTC-5)
+eastern_time = utc_time.astimezone(timezone(timedelta(hours=-5)))
+# Перетворює час UTC в час Східного часового поясу
+print(eastern_time)
+
+print('')
+
+# Припустимо, місцевий час належить до часової зони UTC+2
+local_timezone = timezone(timedelta(hours=2))
+local_time = datetime(year=2023, month=3, day=14, hour=12, minute=30, second=0, tzinfo=local_timezone)
+
+# Конвертація локального часу в UTC
+utc_time = local_time.astimezone(timezone.utc)
+print(utc_time)  # Виведе час в UTC
