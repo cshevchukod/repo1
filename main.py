762defa9d53ebb2deb2902aea5913a6ac3ae4314
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2025-11-10 23:42:29 +0200

    save

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 fh = open('test.txt', 'w')
-fh.write('hello!')
+fh.write('first line\nsecond line\nthird line')
 fh.close()
 
 fh = open('test.txt', 'r')
-all_file = fh.read()
-print(all_file)  # 'hello!'
+while True:
+    line = fh.readline()
+    if not line:
+        break
+    print(line)
 
 fh.close()
+
+with open("test1.txt", "w") as fh:
+    fh.write("first line\nsecond line\nthird line")
+
+with open("test1.txt", "r") as fh:
+    lines = []
+    for el in fh.readlines():
+        lines.append(el.strip())
+
+print(lines)
+
+# Перетворення списку чисел у байт-рядок
+numbers = [0, 128, 255]
+byte_numbers = bytes(numbers)
+print(byte_numbers)  # Виведе байтове представлення чисел
+
+s = "Привіт!"
+
+utf8 = s.encode()
+print(f"UTF-8: {utf8}")
+
+utf16 = s.encode("utf-16")
+print(f"UTF-16: {utf16}")
+
+cp1251 = s.encode("cp1251")
+print(f"CP-1251: {cp1251}")
+
+s_from_utf16 = utf16.decode("utf-16")
+print(s_from_utf16 == s)
+
+byte_array = bytearray(b'Kill Bill')
+byte_array[0] = ord('B')
+byte_array[5] = ord('K')
+print(byte_array)
+
+print('')
+
+python echo.py test --user -hello
